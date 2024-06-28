a456311b95566cba2b4ea6fbcbb35eb9891bf0db
unknown <kumarmanove2003@gmail.com>
2024-06-28 15:09:26 +0500

    commit all last

--- index.js
+++ index.js
@@ -8,16 +8,21 @@ const cors=require('cors');
 // Load environment variables from .env file
 dotenv.config();
 const app = express();
+const allowedOrigins = ["http://localhost:5173"];
+
+const credentials = (req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Credentials", true);
+  }
+  next();
+};
+app.use(credentials);
+app.use(cors(corsOptions));
 const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(bodyParser.json());
-app.use(cors());
-app.use(cors({
-    origin: 'https://ghost-shooter-game.netlify.app/',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow only specified methods
-    allowedHeaders: ['Content-Type', 'Authorization']
-  }));
 // Connect to MongoDB using the connection string from environment variables
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
